d63e2f88ef7d5bbdc7c6f5010e9ce32b6614f806
mizota <t.mizota@conol.jp>
2017-10-18 02:21:33 +0900

    gitignoreを反映

--- Pods/Target Support Files/CORONAWriter/CORONAWriter-dummy.m
+++
@@ -1,5 +0,0 @@
-#import <Foundation/Foundation.h>
-@interface PodsDummy_CORONAWriter : NSObject
-@end
-@implementation PodsDummy_CORONAWriter
-@end
--- Pods/Target Support Files/Pods-wifiHelper-admin/Pods-wifiHelper-admin-dummy.m
+++
@@ -1,5 +0,0 @@
-#import <Foundation/Foundation.h>
-@interface PodsDummy_Pods_wifiHelper_admin : NSObject
-@end
-@implementation PodsDummy_Pods_wifiHelper_admin
-@end
